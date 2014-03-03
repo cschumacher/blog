cd40b05cd5fa5b537b13ce2478f7502957652064
Curtis <schumacr@mail.uc.edu>
2014-03-03 13:10:49 -0500

    Add new comment model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140303054934) do
+ActiveRecord::Schema.define(version: 20140303180631) do
+
+  create_table "commenters", force: true do |t|
+    t.string   "commenter"
+    t.text     "body"
+    t.integer  "post_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "commenters", ["post_id"], name: "index_commenters_on_post_id"
 
   create_table "posts", force: true do |t|
     t.string   "title"
